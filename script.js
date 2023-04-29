d90dd3997c3db917a1ff363c4fa2ef81f93d51d9
shrey-Odinproject <sonishreyf1@gmail.com>
2023-04-29 19:40:21 +0530

    Create Library, a book constructor, a function to add books to library and display all books in library

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-alert('o')
+let Library = [];
+
+function Book(title, author, pgnum, read = false) {
+    this.title = title
+    this.author = author
+    this.pgnum = pgnum
+    this.read = read
+
+    this.info = function () {
+        return `${title} by ${author} has ${pgnum} pages and is ${read}`
+    }
+}
+
+function addBookToLibrary(book) {
+    Library.push(book)
+}
+
+function displayLibrary () {
+    for (book of Library) {
+        displayBook (book)
+    }
+}
+
+function displayBook (book) {
+    let div = document.getElementsByClassName('library')[0].appendChild(document.createElement('div'))
+    div.classList.add('book')
+    div.innerHTML = book.info()
+}
+
+const book1 = new Book('Star Wars', 'George Lucas', 345, true)
+const book2 = new Book('Laura: The Adventure', 'Läusel Keil', 35, false)
+const book3 = new Book('Apfel essen leicht gemacht', 'Läusel Keilo', 5601, false)
+
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+
+displayLibrary()
